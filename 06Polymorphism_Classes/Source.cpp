bf0b8c0e32e083aaedcb58bc5236c4ee4bd3f3c3
tizjordan@gmail.com <tizjordan@gmail.com>
2021-04-22 16:39:17 -0700

    New

--- 06Polymorphism_Classes/Source.cpp
+++ 06Polymorphism_Classes/Source.cpp
@@ -2,9 +2,13 @@
 #include <fstream>
 #include <iomanip>
 
+#include "Parcel.h"
+
 using namespace std;
 
 int main() {
+  const int MAX_PARCELS = 25;
+  Parcel *apcParcel[MAX_PARCELS];
   ifstream inputFile;
 
   inputFile.open("parcels.txt");
