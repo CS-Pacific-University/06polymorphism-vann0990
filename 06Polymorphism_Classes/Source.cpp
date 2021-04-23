d68bcf7aa82e64792c50cb24e3aaf61f2058bb00
tizjordan@gmail.com <tizjordan@gmail.com>
2021-04-22 17:32:50 -0700

    New

--- 06Polymorphism_Classes/Source.cpp
+++ 06Polymorphism_Classes/Source.cpp
@@ -18,9 +18,15 @@ Parcel* apcParcel[MAX_PARCELS];
 const char LETTER = 'L';
 const char POSTCARD = 'P';
 const char OVERNIGHT = 'O';
+const char OPTION_1 = '1';
+const char OPTION_2 = '2';
+const char OPTION_3 = '3';
+const char OPTION_4 = '4';
+const char OPTION_5 = '5';
 
 int main() {
   ifstream inputFile;
+  string menuChoice;
 
   inputFile.open("parcels.txt");
   readFile(inputFile);
